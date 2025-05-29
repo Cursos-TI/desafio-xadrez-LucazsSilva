ae70dd1f626b313d7a5ef1e93bd2d4e9e9d94af5
Lucas da Silva <133595928+LucazsSilva@users.noreply.github.com>
2025-05-29 16:38:30 +0000

    Nivel mestre - Criando Movimentos Complexos

--- xadrez.c
+++ xadrez.c
@@ -4,34 +4,54 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-
-    int movimentacaoTorre = 0;
-    int movimentacaoBispo = 0;
-    int movimentacaoRainha = 0;
-    int movimentacaoCavalo = 1;
+void funcaoTorre() {
 
     // Movimento da Torre cinco casas para a direita.
+    int movimentacaoTorre = 0;
     printf("====== MOVIMENTACAO TORRE ======\n");
     for (int movimentacaoTorre = 0; movimentacaoTorre < 5; movimentacaoTorre++){
         printf("Direita\n");
     }
 
-    // Movimento da Bispo cinco casas para a Cima, Direita.
-    printf("====== MOVIMENTACAO BISPO ======\n");
-    while (movimentacaoBispo < 5){
-        printf("Cima, direita\n");
-        movimentacaoBispo++;
+}
 
+
+void funcaoBispo(){
+    // Movimento da Bispo cinco casas para a Cima, Direita.
+
+    int movimentacaoBispo = 0;
+    
+    printf("====== MOVIMENTACAO BISPO ======\n");
+    for (movimentacaoBispo = 0; movimentacaoBispo < 5; movimentacaoBispo++){
+        for (int j = 0; j < 5; j++){
+            if (movimentacaoBispo == j){
+                printf("Cima, direita\n");
+            }
+        }
+        
     }
 
+}
+
+void funcaoRainha(){
     // Movimento da Rainha cinco casas para a esquerda.
+    int movimentacaoRainha = 0;
+    
     printf("====== MOVIMENTACAO RAINHA ======\n");
     do {
         movimentacaoRainha++;
         printf("Esquerda\n");
     } while (movimentacaoRainha < 8);
+
+}
+
+int main() {
+
+   funcaoTorre();
+   funcaoBispo();
+   funcaoRainha();
+
+    int movimentacaoCavalo = 1;
 
     // Movimento do cavelo.
     printf("====== MOVIMENTACAO CAVALO ======\n");
@@ -39,9 +59,9 @@ int main() {
     
     while (movimentacaoCavalo--){
         for (int j = 1; j <= 2; j++){
-            printf("Baixo\n");
+            printf("Cima\n");
         }
-        printf("Esquerda\n");
+        printf("Direita\n");
     }
 
     return 0;
